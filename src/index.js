07731f502b639cc27c311e580c8a487ec58ca21a
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-14 18:32:47 +0530

    Add PostgreSQL connection pool

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT: ", PORT);
