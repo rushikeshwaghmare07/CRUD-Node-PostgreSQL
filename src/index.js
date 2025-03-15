aba8cff5e6f1a18f69e6e1a11bfac7cee1c75cc3
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-15 22:53:46 +0530

    Update routes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 
 // Create user table
 createUserTable();
